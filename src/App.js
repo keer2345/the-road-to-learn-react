a06a7f1f476de9d2c8281d87fe38aedafd65c1d6
keer2345 <keer2345@gmail.com>
2018-10-10 22:56:42 +0800

    ES6解构

--- src/App.js
+++ src/App.js
@@ -51,12 +51,13 @@ class App extends Component {
   }
 
   render() {
+    const { list, searchTerm } = this.state;
     return (
       <div className="App">
         <form>
           <input type="text" onChange={this.onSearchChange} />
         </form>
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => (
+        {list.filter(isSearched(searchTerm)).map(item => (
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
